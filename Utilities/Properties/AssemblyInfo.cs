df698034489642d95fd5ca4995f66dac857d4bee
zeev abramovich <57068932+zeviAbramovich@users.noreply.github.com>
2019-12-16 17:09:09 +0200

    fdtg

--- Utilities/Properties/AssemblyInfo.cs
+++ Utilities/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("utikijj")]
+[assembly: AssemblyTitle("Utilities")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("utikijj")]
+[assembly: AssemblyProduct("Utilities")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e2b86a1f-e035-4212-9dbd-cbbcf370bee2")]
+[assembly: Guid("81e7d3b5-0592-428c-a16b-4acdae92e69f")]
 
 // Version information for an assembly consists of the following four values:
 //
