39528e67f71390ee85645768b76a48722baeb2e5
binim storch <binim1010@gmail.com>
2020-01-17 16:35:01 +0200

    WPFF

---
+++ PLWPF/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using ShowMeTheXAML;
+using MaterialDesignThemes;
+using MaterialDesignColors;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PLWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            XamlDisplay.Init();
+            base.OnStartup(e);
+        }
+    }
+   
+}
